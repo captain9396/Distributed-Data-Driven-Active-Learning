69a6122d9b86e1c978a13b10fcdd9b65e0de5dad
dv66 <mostofamonsur9396@gmail.com>
2019-03-23 19:16:27 +0000

    similarity calculation with columnsimilarities

--- final_thesis/test.py
+++ final_thesis/test.py
@@ -2,7 +2,11 @@ from pyspark import  SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.util import MLUtils
+from  pyspark.mllib.linalg import SparseVector, DenseVector
 from sklearn.preprocessing import normalize
+from pyspark.mllib.linalg.distributed import IndexedRow, IndexedRowMatrix, BlockMatrix, MatrixEntry, RowMatrix, CoordinateMatrix
+from pyspark.mllib.linalg import Matrix, Matrices, DenseMatrix
+from pyspark.mllib.feature import Normalizer
 import numpy as np
 # setup spark context and config
 conf = SparkConf().setAppName("labeledPoints")
@@ -13,12 +17,18 @@ sc = SparkContext(conf=conf)
 
 data = sc.textFile('hdfs://node1:9000/input/data.txt')
 data = data.map(lambda _ : np.array(_.strip().split()).astype(float))
-data = data.map(lambda _ : _/np.linalg.norm(_))
+unitMatrix = data.map(lambda _ : _/np.linalg.norm(_))
+
+unitMatrix = sc.parallelize(np.array([[1,2,3,5,7], [6,2,1,-1,3], [7,0,1,2,-4]]).T)
+mat = RowMatrix(unitMatrix)
+S = mat.columnSimilarities()
+
+sims = S.entries.collect()
+print(len(sims))
+print(sims)
 
 
-print(data.take(1))
-
-
-
+SrowMatrix = S.toRowMatrix()
+print(SrowMatrix.rows.map(lambda _ : DenseVector(_.toArray())).collect())
 
 
