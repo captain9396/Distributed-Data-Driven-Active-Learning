8d658498a44a83f9b3b342c96ebd1795ead0d33b
Syed Mostofa Monsur <mostofamonsur9396@gmail.com>
2019-04-18 21:43:46 +0600

    Create similarity.py

---
+++ final_thesis/similarity.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+from debugger import Debugger
+from pyspark import  SparkConf, SparkContext
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
+from  pyspark.mllib.linalg import SparseVector, DenseVector
+from sklearn.preprocessing import normalize
+from pyspark.mllib.linalg.distributed import IndexedRow, IndexedRowMatrix, BlockMatrix, MatrixEntry, RowMatrix, CoordinateMatrix
+from pyspark.mllib.linalg import Matrix, Matrices, DenseMatrix
+from pyspark.mllib.feature import Normalizer
+import numpy as np
+# setup spark context and config
+conf = SparkConf().setAppName("labeledPoints")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+debug = Debugger()
+debug.TIMESTAMP(1)
+spark = SparkSession(sc)
+
+
+
+
+
+data = sc.textFile('hdfs://node1:9000/input/vectors_3000x500.txt')
+data = data.map(lambda _ : np.array(_.strip().split()).astype(float))
+data = data.map(lambda _ : _/np.linalg.norm(_))
+irmat = data.zipWithIndex().map(lambda _ : IndexedRow(_[1], _[0]))
+irmat = IndexedRowMatrix(irmat)
+
+
+
+comat = irmat.toCoordinateMatrix()
+comat = comat.transpose()
+
+irmat = comat.toIndexedRowMatrix()
+simi = irmat.columnSimilarities()
+
+#simi_list = simi.entries.collect()
+#print(len(simi_list))
+#print(simi_list)
+print(simi.entries.take(10))
+
+
+debug.TIMESTAMP(2)
